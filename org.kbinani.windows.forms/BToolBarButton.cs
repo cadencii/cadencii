003af969e9a423cadb433fc4bc9e9541cb56ffe0
kbinani <kbinani@9d680d4f-cbfa-477e-9905-73dbafb9b024>
2011-01-15 21:30:16 +0000

    [Cadencii/trunk] java版のための変更(未完)‏
    git-svn-id: https://svn.sourceforge.jp/svnroot/cadencii/Cadencii/trunk@1456 9d680d4f-cbfa-477e-9905-73dbafb9b024

---
+++ org.kbinani.windows.forms/BToolBarButton.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * BToolBarButton.cs
+ * Copyright © 2011 kbinani
+ *
+ * This file is part of org.kbinani.windows.forms.
+ *
+ * org.kbinani.windows.forms is free software; you can redistribute it and/or
+ * modify it under the terms of the BSD License.
+ *
+ * org.kbinani.windows.forms is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+#if JAVA
+//INCLUDE ../BuildJavaUI/src/org/kbinani/windows/forms/BToolBarButton.java
+#else
+namespace org.kbinani.windows.forms
+{
+
+    public class BToolBarButton : System.Windows.Forms.ToolBarButton
+    {
+        public bool isCheckOnClick()
+        {
+            return (base.Style == System.Windows.Forms.ToolBarButtonStyle.ToggleButton);
+        }
+
+        public void setCheckOnClick( bool value )
+        {
+            base.Style = System.Windows.Forms.ToolBarButtonStyle.ToggleButton;
+        }
+
+        public bool isSelected()
+        {
+            return base.Pushed;
+        }
+
+        public void setSelected( bool value )
+        {
+            base.Pushed = value;
+        }
+    }
+
+}
+#endif
