b9f1f395e2eb765e2cf8a907a7530fa1ac6500d3
kbinani <kbinani@9d680d4f-cbfa-477e-9905-73dbafb9b024>
2011-08-18 17:38:32 +0000

    [Cadencii/branches/3.4] #25985 C#の分をtrunkから移設
    git-svn-id: https://svn.sourceforge.jp/svnroot/cadencii/Cadencii/branches/3.4@1736 9d680d4f-cbfa-477e-9905-73dbafb9b024

---
+++ Cadencii/PureVirtualFunctionAttribute.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * PureVirtualFunctionAttribute.cs
+ * Copyright © 2011 kbinani
+ *
+ * This file is part of org.kbinani.cadencii.
+ *
+ * org.kbinani.cadencii is free software; you can redistribute it and/or
+ * modify it under the terms of the GPLv3 License.
+ *
+ * org.kbinani.cadencii is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+#if CSHARP
+using System;
+
+namespace org.kbinani.cadencii
+{
+
+    /// <summary>
+    /// メソッドが純粋仮想関数であることをpp_cs2javaに通知するための属性です．
+    /// 通知したいメソッドの直前の行にこの属性を記述してください．
+    /// </summary>
+    /// <example>
+    /// C#のコードに以下のように記述したとします．
+    /// <code>
+    /// interface Foo{
+    ///     [PureVirtualFunction]
+    ///     void someMethod();
+    /// }
+    /// </code>
+    /// すると，pp_cs2javaに--replace-cppオプションを付けて処理させると次のようになります．
+    /// <code>
+    /// interface Foo{
+    ///     virtual void someMethod() = 0;
+    /// }
+    /// </code>
+    /// </example>
+    public class PureVirtualFunctionAttribute : Attribute { }
+
+}
+#endif
