7ccbde8fb9e6cca97e0b67b453d81601e306d354
kbinani <kbinani@9d680d4f-cbfa-477e-9905-73dbafb9b024>
2011-08-20 14:03:54 +0000

    [Cadencii/branches/3.4] Cadencii_v3.4.1
    git-svn-id: https://svn.sourceforge.jp/svnroot/cadencii/Cadencii/branches/3.4@1739 9d680d4f-cbfa-477e-9905-73dbafb9b024

--- Cadencii/BAssemblyInfo.cs
+++ Cadencii/BAssemblyInfo.cs
@@ -40,7 +40,7 @@ namespace org.kbinani.cadencii {
         public const String id = "$Id$";
         public const String fileVersionMeasure = "3";
         public const String fileVersionMinor = "4";
-        public const String fileVersion = fileVersionMeasure + "." + fileVersionMinor + ".0";
+        public const String fileVersion = fileVersionMeasure + "." + fileVersionMinor + ".1";
     }
 
 #if !JAVA
