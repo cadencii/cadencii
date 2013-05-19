4c16419b43907c5d3ef8aaf728cff0449df64743
kbinani <kbinani.bt@gmail.com>
2013-05-19 20:15:20 +0900

    Modify plugin/ScriptProcessor.cs, refactor to support renamed namespace

--- Cadencii/plugin/ScriptProcessor.cs
+++ Cadencii/plugin/ScriptProcessor.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
-using ICSharpCode.NRefactory.CSharp;
 using System.Linq;
 
 namespace cadencii
@@ -16,69 +15,20 @@ namespace cadencii
         public string process( string code )
         {
             var targetCode = getPrefix() + code + getSuffix();
-            var parser = new CSharpParser();
-            var root = parser.Parse( targetCode, "" );
-            root
-                .Descendants
-                .OfType<Identifier>()
-                .Where( ( id ) => { var n = GetSiblingNode( id, 1 ); return n != null && n.GetType() == typeof( CSharpTokenNode ); } )
-                .ToList().ForEach( ( id ) => {
-                    var childIdentifier = GetSiblingNode( id, 2 ) as Identifier;
-                    var grandChildIdentifier = GetSiblingNode( id, 4 ) as Identifier;
-                    if ( GetSiblingNode( id, 3 ) != null && GetSiblingNode( id, 3 ).GetType() != typeof( CSharpTokenNode ) ) {
-                        grandChildIdentifier = null;
-                    }
-                    if ( (id.Name == "Boare" && childIdentifier.Name == "Lib") ||
-                         (id.Name == "org" && childIdentifier.Name == "kbinani") ) {
-                        id.Name = "com";
-                        if ( grandChildIdentifier != null && grandChildIdentifier.Name.ToLower() != "cadencii" ) {
-                            childIdentifier.Name = "github.cadencii";
-                        } else {
-                            childIdentifier.Name = "github";
-                        }
-                        if ( grandChildIdentifier != null ) {
-                            grandChildIdentifier.Name = grandChildIdentifier.Name.ToLower();
-                        }
-                    }
-
-                    if ( id.Name == "Boare" && childIdentifier.Name == "Cadencii" ) {
-                        id.Name = "com";
-                        childIdentifier.Name = "github.cadencii";
-                    }
-                    if ( id.Name == "bocoree" ) {
-                        id.Name = "cadencii.java";
-                    }
-                    if ( id.Name == "bocoreex" ) {
-                        id.Name = "cadencii.javax";
-                    }
-                    if ( id.Name == "InputBox" ) {
-                        id.Name = typeof( cadencii.windows.forms.InputBox ).FullName;
-                    }
-                } );
-            root
-                .Descendants
-                .ToList()
-                .ForEach( ( node ) => {
-                    if ( node.Children.Count() == 0 ) {
-                        if ( node.GetText() == "InputBox" && node.GetNextNode().GetType() != typeof( CSharpTokenNode ) ) {
-                            (node as Identifier).Name = typeof( cadencii.windows.forms.InputBox ).FullName;
-                        }
-                    }
-                } );
-            return root.GetText();
-        }
-
-        AstNode GetSiblingNode( AstNode node, int count )
-        {
-            if ( count == 0 ) {
-                return node;
-            } else {
-                if ( node == null ) {
-                    return null;
-                } else {
-                    return GetSiblingNode( node.GetNextNode(), count - 1 );
-                }
-            }
+            return targetCode
+                .Replace( "Boare.Lib.Vsq", "cadencii.vsq" )
+                .Replace( "Boare.Lib.AppUtil", "cadencii.apputil" )
+                .Replace( "Boare.Lib.Media", "cadencii.media" )
+                .Replace( "Boare.Cadencii", "cadencii" )
+                .Replace( "org.kbinani.vsq", "cadencii.vsq" )
+                .Replace( "org.kbinani.apputil", "cadencii.apputil" )
+                .Replace( "org.kbinani.cadencii", "cadencii" )
+                .Replace( "org.kbinani.java", "cadencii.java" )
+                .Replace( "org.kbinani", "cadencii" )
+                .Replace( "bocoreex", "cadencii.javax" )
+                .Replace( "bocoree", "cadencii.java" )
+                .Replace( "InputBox", typeof( cadencii.windows.forms.InputBox ).FullName )
+                ;
         }
     }
 
