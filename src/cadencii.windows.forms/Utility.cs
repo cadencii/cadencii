3c0f264bd76fe4ba8d6075f604d1a71d0fc95015
kbinani <kbinani.bt@gmail.com>
2013-09-28 01:51:10 +0900

    Fix bug: crash in open/save file dialog

--- src/cadencii.windows.forms/Utility.cs
+++ src/cadencii.windows.forms/Utility.cs
@@ -47,7 +47,7 @@ namespace cadencii.windows.forms {
         public static System.Windows.Forms.FileDialog SetSelectedFile(this System.Windows.Forms.FileDialog dialog, string file_path)
         {
             string file_name = string.Empty;
-            string initial_directory = System.IO.Directory.Exists(file_path) ? file_path : System.IO.Path.GetDirectoryName(file_path);
+            string initial_directory = System.IO.Directory.Exists(file_path) ? file_path : (System.IO.File.Exists(file_path) ? System.IO.Path.GetDirectoryName(file_path) : file_path);
 
             dialog.FileName = file_name;
             dialog.InitialDirectory = initial_directory;
