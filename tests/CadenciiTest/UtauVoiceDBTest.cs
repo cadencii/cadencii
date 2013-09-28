b6efd35d601f32c95549b1d3f26681b5f267708f
kbinani <kbinani.bt@gmail.com>
2013-09-28 21:20:37 +0900

    [CadenciiTest] Add UtauVoiceDBTest

---
+++ tests/CadenciiTest/UtauVoiceDBTest.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using cadencii;
+using cadencii.vsq;
+using cadencii.utau;
+
+namespace cadencii.test
+{
+    [TestFixture]
+    class UtauVoiceDBTest
+    {
+        [Test]
+        public void test()
+        {
+            var config = new SingerConfig();
+            config.VOICEIDSTR = "./fixture/utau_voice_db";
+            var db = new UtauVoiceDB(config);
+            var actual = db.attachFileNameFromLyric("‚ ");
+            Assert.AreEqual("‚ ", actual.Alias);
+            Assert.AreEqual("‚ .wav", actual.fileName);
+            Assert.AreEqual(6f, actual.msOffset);
+            Assert.AreEqual(52f, actual.msConsonant);
+            Assert.AreEqual(69f, actual.msBlank);
+            Assert.AreEqual(1f, actual.msPreUtterance);
+            Assert.AreEqual(2f, actual.msOverlap);
+        }
+    }
+}
