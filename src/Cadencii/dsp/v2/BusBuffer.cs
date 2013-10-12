339d21be08eb948c38aec82d8898ae4ae869c81d
kbinani <kbinani.bt@gmail.com>
2013-10-12 18:03:51 +0900

    [Cadencii] Add BusBuffer class

---
+++ src/Cadencii/dsp/v2/BusBuffer.cs
@@ -0,0 +1,89 @@
+/*
+ * BusBuffer.cs
+ * Copyright © 2013 kbinani
+ *
+ * This file is part of cadencii.
+ *
+ * cadencii is free software; you can redistribute it and/or
+ * modify it under the terms of the GPLv3 License.
+ *
+ * cadencii is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+using System.Diagnostics;
+
+namespace cadencii.dsp.v2
+{
+    /// <summary>
+    /// float, n-channel, interleaved wave buffer.
+    /// </summary>
+    public class BusBuffer
+    {
+        /// <summary>
+        /// Raw buffer data.
+        /// </summary>
+        protected float[] buffer_;
+        private readonly int length_;
+        private readonly int channel_;
+
+        /// <summary>
+        /// Initialize buffer.
+        /// </summary>
+        /// <param name="channel">The number of channels.</param>
+        /// <param name="length">The buffer length of the buffer for each channel.</param>
+        public BusBuffer(int channel, int length)
+        {
+            Debug.Assert(channel > 0);
+            Debug.Assert(length > 0);
+            length_ = length;
+            channel_ = channel;
+            buffer_ = new float[channel_ * length_];
+        }
+
+        /// <summary>
+        /// The length of the buffer.
+        /// </summary>
+        public int Length { get { return length_; } }
+
+        /// <summary>
+        /// The number of channels.
+        /// </summary>
+        public int Channel { get { return channel_; } }
+
+        /// <summary>
+        /// Fill buffer with 0.0f.
+        /// </summary>
+        /// <param name="offset">The offset index to be filled.</param>
+        /// <param name="length">The length to be filled.</param>
+        public void zeroFill(int offset, int length)
+        {
+            int actual_offset = channel_ * offset;
+            int actual_length = channel_ * length;
+            for (int i = actual_offset; i < actual_offset + actual_length; ++i) {
+                buffer_[i] = 0.0f;
+            }
+        }
+
+        /// <summary>
+        /// Mix wave buffer data from the source.
+        /// </summary>
+        /// <param name="source">Source buffer to mix.</param>
+        /// <param name="source_offset">Start index of source buffer.</param>
+        /// <param name="offset">Start index of this buffer.</param>
+        /// <param name="length">The sample length to be mixed.</param>
+        public void mixFrom(BusBuffer source, int source_offset, int offset, int length)
+        {
+            Debug.Assert(Channel == source.Channel);
+            Debug.Assert(0 <= source_offset && source_offset + length < source.Length);
+            Debug.Assert(0 <= offset && offset + length < Length);
+            int channel = source.Channel;
+            int actual_soruce_offset = channel * source_offset;
+            int actual_offset = channel * offset;
+            int actual_length = channel * length;
+            for (int i = 0; i < actual_length; ++i) {
+                buffer_[actual_offset + i] += source.buffer_[actual_soruce_offset + i];
+            }
+        }
+    }
+}
---
+++ tests/CadenciiTest/dsp/BusBufferTest.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using cadencii;
+using cadencii.dsp.v2;
+using NUnit.Framework;
+
+namespace cadencii.test.dsp
+{
+    [TestFixture]
+    class BusBufferTest
+    {
+        class BusBufferStub : BusBuffer
+        {
+            public BusBufferStub(int channel, int buffer_size)
+                : base(channel, buffer_size)
+            { }
+
+            public float[] getBuffer()
+            {
+                return buffer_;
+            }
+        }
+
+        [Test]
+        public void zeroFillTest()
+        {
+            var buffer = new BusBufferStub(2, 412);
+
+            // fill dummy data
+            const float DUMMY_DATA = 0.4f;
+            float[] raw_buffer = buffer.getBuffer();
+            Assert.True(raw_buffer.All((_1) => _1 == 0.0f));
+            for (int i = 0; i < raw_buffer.Length; ++i) {
+                raw_buffer[i] = DUMMY_DATA;
+            }
+
+            buffer.zeroFill(1, 410);
+
+            Assert.AreEqual(raw_buffer[0], DUMMY_DATA);
+            Assert.AreEqual(raw_buffer[1], DUMMY_DATA);
+            Assert.True(raw_buffer.Skip(2).Take(410 * 2).All((_1) => _1 == 0.0f));
+            Assert.AreEqual(raw_buffer[raw_buffer.Length - 2], DUMMY_DATA);
+            Assert.AreEqual(raw_buffer[raw_buffer.Length - 1], DUMMY_DATA);
+        }
+
+        [Test]
+        public void zeroFillWithZeroLengthTest()
+        {
+            var buffer = new BusBuffer(2, 100);
+            Assert.DoesNotThrow(() => buffer.zeroFill(0, 0));
+            Assert.DoesNotThrow(() => buffer.zeroFill(0, -1));
+        }
+
+        [Test]
+        public void mixFromTest()
+        {
+            const int CHANNEL = 2;
+            const int LENGTH = 100;
+
+            var buffer = new BusBufferStub(CHANNEL, LENGTH);
+            var raw_buffer = buffer.getBuffer();
+            fillRandomData(raw_buffer);
+
+            var source = new BusBufferStub(CHANNEL, LENGTH);
+            var source_raw_buffer = source.getBuffer();
+            fillRandomData(source_raw_buffer);
+
+            const int SOURCE_OFFSET = 10;
+            const int OFFSET = 20;
+            const int MIX_LENGTH = 10;
+
+            float[] expected = new float[CHANNEL * LENGTH];
+            for (int i = 0; i < raw_buffer.Length; ++i) {
+                expected[i] = raw_buffer[i];
+            }
+            for (int i = 0; i < MIX_LENGTH; ++i) {
+                for (int ch = 0; ch < CHANNEL; ++ch) {
+                    expected[i * CHANNEL + OFFSET * CHANNEL + ch] += source_raw_buffer[i * CHANNEL + SOURCE_OFFSET * CHANNEL + ch];
+                }
+            }
+
+            buffer.mixFrom(source, SOURCE_OFFSET, OFFSET, MIX_LENGTH);
+            float[] actual = buffer.getBuffer();
+
+            bool all_equals = true;
+            for (int i = 0; i < expected.Length; ++i) {
+                if (expected[i] != actual[i]) {
+                    all_equals = false;
+                    break;
+                }
+            }
+            Assert.True(all_equals);
+        }
+
+        private void fillRandomData(float[] data)
+        {
+            var rand = new Random();
+            for (int i = 0; i < data.Length; ++i) {
+                data[i] = (float)rand.NextDouble();
+            }
+        }
+    }
+}
